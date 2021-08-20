6dd8573bdb6fb3104e794c0fad845851dbdf11a5
stephendltg <s.deletang@laposte.net>
2021-08-20 16:03:48 +0200

    fix(bundle): fix

--- nuxt.config.js
+++ nuxt.config.js
@@ -24,7 +24,7 @@ export default {
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: package.description },
+      { hid: 'description', name: 'description', content: pkg.description },
       { name: 'format-detection', content: 'telephone=no' }
     ],
     link: [
