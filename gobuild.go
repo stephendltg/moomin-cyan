6a60c571a28f899c96b5ddd138efe9aac8d775de
stephendltg <s.deletang@laposte.net>
2021-08-25 10:17:50 +0200

    Update gobuild.go

--- gobuild.go
+++ gobuild.go
@@ -1,11 +1,33 @@
 package main
 
 import (
+  "encoding/json"
+  "io/ioutil"
 	"fmt"
 	"runtime"
+  "strconv"
 )
 
+// package json data
+type Data struct {
+    Name   string `json:"name"`
+    Description   string `json:"description"`
+    Version    string    `json:"version"`
+}
+
 func main() {
+  
+  // Read package.json
+  jsonPackage, err := os.Open("package.json")
+  if err != nil {
+     panic(err)
+  }
+  defer jsonPackage.Close()
+  byteValue, _ := ioutil.ReadAll(jsonPackage)
+  var data Data
+  json.Unmarshal(byteValue, &data)
+                                 
+  fmt.Println("Data name: " + data.name)
 
 	if runtime.GOOS == "darwin" {
 		fmt.Println("mac")
